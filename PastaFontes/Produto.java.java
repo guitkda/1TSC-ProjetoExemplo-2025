77d2b1dc3247a6992001838e68753a01613265f9
guitkda <gui.takada10@gmail.com>
2025-04-14 11:34:44 -0300

    Mudaca do tipo da variavel peso

--- PastaFontes/Produto.java.java
+++ PastaFontes/Produto.java.java
@@ -3,19 +3,19 @@
 
 public class Produto {
 
-	private double peso;
+	private int peso;
 	private double altura;
 	
 	public Produto() {
 		
 	}
 	
-	public Produto(double peso, double altura) {
+	public Produto(int peso, double altura) {
 		this.peso=peso;
 		this.altura=altura;
 	}
 	
-	public double getPeso() {
+	public int getPeso() {
 		return peso;
 	}
 	
